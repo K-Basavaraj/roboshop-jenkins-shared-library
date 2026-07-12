26370471ebbd027d513b45f41e2eecb3d5f830cb
KuruvaBasavaraja <kuruva.basavaraja09@gmail.com>
2026-07-12 15:58:20 +0530

    update

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,16 +1,23 @@
-def getAccountID(String environment){
-    switch(environment) { 
-        case 'dev': 
+def getAccountID(String environment) {
+    println "DEBUG: getAccountID called with ${environment}"
+
+    switch(environment) {
+        case 'dev':
             return "688567303455"
+
         case 'qa':
             return "315069554701"
+
         case 'uat':
             return "315069644701"
+
         case 'pre-prod':
             return "315069354701"
+
         case 'prod':
             return "315063654701"
+
         default:
-            return "Unknown environment: ${environment}"
-    } 
+            error "Unknown environment: ${environment}"
+    }
 }
